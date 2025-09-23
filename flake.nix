d159e456a4fa8be97cd355a6b973564ef07af7d9
Patrick Rall <patrickwrall@gmail.com>
2025-09-22 23:22:34 -0400

    feat(root): nix devshell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Patrick Rall Site";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          default = pkgs.callPackage ./shell.nix { };
+        });
+    };
+}
